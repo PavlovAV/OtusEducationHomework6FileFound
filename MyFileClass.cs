3fceb7f8b02570787cc6d733d2d7a04c3bb900db
PavlovAV <a.pavlov@sgbi.ru>
2023-05-06 22:15:25 +0300

    Add project files.

---
+++ MyFileClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OtusEducationHomework6
+{
+    internal class MyFileClass
+    {
+        public event EventHandler<FileArgs> FileFound;
+
+        public void Run(string path)
+        {
+            if (Directory.Exists(path))
+                {
+                foreach (var file in Directory.GetFiles(path))
+                {
+                    var fileArgs = new FileArgs(file);
+                    FileFound.Invoke(this, fileArgs);
+                    if (fileArgs.Exit)
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OtusEducationHomework6;
+
+Console.WriteLine("Hello, World!");
+
+var myFileClass = new MyFileClass();
+myFileClass.FileFound += FileFound;
+myFileClass.Run(@"D:\");
+
+static void FileFound(object sender, FileArgs e)
+{
+    Console.WriteLine($"File {e} found");
+    Console.WriteLine("Continue (Y/N)?");
+    e.Exit = Console.ReadKey().KeyChar == 'N';
+    Console.WriteLine();
+}
+
+public class FileArgs : EventArgs
+{
+	string FileName;
+    public bool Exit { get; set; }
+
+	public FileArgs(string? fileName)
+	{
+		FileName = fileName ?? string.Empty;
+        Exit = false;
+	}
+    public override string ToString()
+    {
+        return FileName;
+    }
+}
